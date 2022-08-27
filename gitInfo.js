2b45220322c591d2a35f66ad67c45faaf2c49837
Jeremy-Moss <mosstoggle@gmail.com>
2022-08-27 17:21:33 -0400

    gitInfo finished

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+const gitDefinition = ("Git is a program that creates local repositories that can be pushed to a version control website that we use called github.");
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+const gitHubDefinition = ("Github is a website that can be used for version control that we will use to store git files, and organize them using separate commits. Just in case we need to go back to a previous version of our software.");
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+const gitInitDefinition = ("Git Init is the first command that we use to initiate a local repository for the files that we have open in our editor at that point");
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+const gitCloneDefinition = ("Git clone is a command that we can use make a copy of a repository and open it in a new location");
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+const gitStatus = ("Git status is a command that we can use to check which files are and are not being tracked by git");
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,8 @@
 */
 
 //CODE HERE
-
+const gitAddDefinition = ("Git add is a command that we use to place a repository into a staging area where they are ready to commit, we can add all or specific files. ");
+const gitAddCode = ("git add .")
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,8 +65,8 @@
 */
 
 //CODE HERE
-
-
+const gitCommitDefinition = ("git commit is a snapshot of our repository at a specific point in time, giving us the ability to see how the file has changed, as well as go back to a previous version if we need to for any reason");
+const gitCommitCode = ('git commit -m "initial commit"')
 //////////////////PROBLEM 8////////////////////
 /*
     Create a variable called 'gitPushDefinition'.  
@@ -73,3 +74,4 @@
 */
 
 //CODE HERE
+const gitPushDefinition = ("Git push is the command we use to upload our repository to gitHub or any other website that can remotely track our git repositories")
